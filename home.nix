fb9754a8b4e80b152904e790ef682e3bcb1dbc4f
Martin Bieker <martin.bieker@udo.edu>
2025-03-15 17:18:29 +0100

    Add home manager and hardware config

---
+++ home.nix
@@ -0,0 +1,44 @@
+{ lib, pkgs, ... }: {
+  home = {
+    # Install packages from https://search.nixos.org/packages
+    packages = with pkgs; [
+      zoom-us
+      cowsay
+      thunderbird
+      telegram-desktop
+      mattermost-desktop
+      inkscape
+      expect
+      nextcloud-client
+      jabref
+    ];
+
+    # This needs to be set to your actual username.
+    username = "msb";
+    homeDirectory = "/home/msb";
+
+    # Don't ever change this after the first build.
+    # It tells home-manager what the original state schema
+    # was, so it knows how to go to the next state.  It
+    # should NOT update when you update your system!
+    stateVersion = "24.11";
+  };
+  programs = {
+    git = {
+      enable = true;
+      userName = "Martin Bieker";
+      userEmail = "martin.bieker@udo.edu";
+    };
+
+    zsh = {
+      enable = true;
+
+      oh-my-zsh = {
+        enable = true;
+        theme = "agnoster";
+      };
+    };
+
+
+  };
+}
