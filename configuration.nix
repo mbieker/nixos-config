ac230a89dd703d0e0740901bce7822f9fbc7dddf
Martin Bieker <martin.bieker@udo.edu>
2025-08-17 20:04:07 +0200

    Use grub bootloader

--- configuration.nix
+++ configuration.nix
@@ -6,8 +6,16 @@
 
 {
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+      grub = {
+      enable = true;
+      devices = [ "nodev" ];
+      efiSupport = true;
+      useOSProber = true;
+      default = "saved";
+    };
+  };
 
   swapDevices = [{
     device = "/swapfile";
