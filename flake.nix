7e37add483fa0e9137e99e2ba5956ffbc7cf638e
Martin Bieker <martin.bieker@udo.edu>
2025-09-04 21:35:21 +0200

    Switch to nixos-umstabple

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # /etc/nixos/flake.nix
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       # The `follows` keyword in inputs is used for inheritance.
