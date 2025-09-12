a87c2c957debd2e70d906044d635c9acdf8d61bd
Martin Bieker <martin.bieker@udo.edu>
2025-09-12 11:33:33 +0200

    Add agthoma gitlab to hosts file

--- configuration.nix
+++ configuration.nix
@@ -49,9 +49,10 @@
   networking.hostName = "msb-t14"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.hosts = {
+  "10.101.36.8" = [ "gitlab-ce.agthoma.hiskp" ] ;
+  };
+
 
   # Enable networking
   networking.networkmanager.enable = true;
