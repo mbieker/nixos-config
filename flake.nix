fb301ca4a7033e8fc0ec1d5facece9d733289a29
Martin Bieker <martin.bieker@uni-bonn.de>
2025-12-10 17:44:55 +0100

    Add workPC home manager config

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [];
+        modules = [./home-manager/work.nix];
   };
 };
 }
