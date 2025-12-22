ce5a1e0187ca91609c7d2cde04758c87a762afa4
Martin Bieker <martin.bieker@udo.edu>
2025-12-22 10:58:31 +0100

    Commit lock file

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
+    url= "github:nix-community/home-manager/release-25.11";
     inputs.nixpkgs.follows = "nixpkgs";
     };
   };
