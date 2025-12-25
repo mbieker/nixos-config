08b8fed80287aad181a3e358c70a1d1c9eb726b9
Martin Bieker <martin.bieker@uni-bonn.de>
2025-12-26 00:02:25 +0100

    Hotfix ssh-agent (gcr vs gnome-keyring)

--- flake.nix
+++ flake.nix
@@ -11,56 +11,68 @@
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
-    url= "github:nix-community/home-manager/release-25.11";
-    inputs.nixpkgs.follows = "nixpkgs";
+      url= "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
-  let
+    let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    nixosConfigurations.msb-t14 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./msb-t14
-        inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.msb = import ./msb-t14/home.nix;
-          }
-      ];
-    };
+    in
+      {
+        nixosConfigurations.msb-t14 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./msb-t14
+            inputs.home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.msb = import ./msb-t14/home.nix;
+            }
+            {
+	            nixpkgs.overlays = [
+                (final: prev: {
+                  cosmic-session = prev.cosmic-session.overrideAttrs (oldAttrs: {
+                    postPatch = (oldAttrs.postPatch or "") + ''
+              substituteInPlace data/start-cosmic \
+                --replace-fail '/keyring/ssh' '/gcr/ssh'
+            '';
+                  });
+                })
+              ];
+            }
+          ];
+        };
 
-    nixosConfigurations.vps = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        {
-	  nixpkgs.overlays = [
-		     inputs.docspell.overlays.default
-	  ];
-	}
-        inputs.disko.nixosModules.disko
-        inputs.docspell.nixosModules.default
-        ./vps
-      ];
-    };
+        nixosConfigurations.vps = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            {
+	            nixpkgs.overlays = [
+		            inputs.docspell.overlays.default
+	            ];
+	          }
+            inputs.disko.nixosModules.disko
+            inputs.docspell.nixosModules.default
+            ./vps
+          ];
+        };
 
-      homeConfigurations."msb-t14" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        homeConfigurations."msb-t14" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home-manager/msb-t14.nix ];
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [ ./home-manager/msb-t14.nix ];
+        };
+        homeConfigurations."work" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [./home-manager/home.nix];
+        };
       };
-      homeConfigurations."work" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [./home-manager/home.nix];
-  };
-};
 }
