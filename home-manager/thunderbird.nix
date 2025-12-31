52b797460ee1490625e6913de602d7b60387de7a
Martin Bieker <martin.bieker@uni-bonn.de>
2025-12-31 17:31:21 +0100

    Add  some more email accounts

--- home-manager/thunderbird.nix
+++ home-manager/thunderbird.nix
@@ -1,12 +1,36 @@
 { config, lib, pkgs, ... }:
 
-{
-  accounts.email.accounts."Uni Bonn" = {
-    primary = true;
+let
+  common = {
+  realName = "Martin Bieker";
 
-    realName = "Martin Bieker";
+
+  thunderbird = {
+    enable = true;
+    profiles = [ "default" ];
+  };
+};
+  mkSignature = address : {
+    showSignature = "attach";
+    text =
+    ''
+      Dr. Martin Bieker, wissenschaftlicher Mitarbeiter
+      Universität Bonn, HISKP, AG Thoma
+      Kreuzbergweg 24, 53115 Bonn, Deutschland
+      Telefon: +49 228 73-69433
+      E-Mail: ${address}
+      www: https://agthoma.hiskp.uni-bonn.de
+      Rheinische Friedrich-Wilhelms-Universität Bonn
+      gegründet 1818, Exzellenzuniversität seit 2019
+    '';
+  };
+  in
+{
+  accounts.email.accounts."Uni Bonn" = rec {
+    primary = true;
     address  = "martin.bieker@uni-bonn.de";
     userName = "mbieker";
+
 
     imap = {
       host = "email.uni-bonn.de";
@@ -19,18 +43,53 @@
       port = 465;
       tls.enable = true;
     };
+    signature = (mkSignature address);
 
-    signature.text =
-      ''
-      --
-      Martin Bieker
-      '';
+  } // common;
 
-    thunderbird = {
-      enable = true;
-      profiles = [ "default" ];
+  accounts.email.accounts."TU Dortmund" = rec {
+    primary = false;
+    address  = "martin.bieker@tu-dortmund.de";
+    userName = "smmnbiek";
+
+
+    imap = {
+      host = "unimail.tu-dortmund.de";
+      port = 993;
+      tls.enable = true;
     };
-  };
+
+    smtp = {
+      host = "unimail.tu-dortmund.de";
+      port = 465;
+      tls.enable = true;
+    };
+    signature = (mkSignature address);
+
+  } // common;
+
+  accounts.email.accounts."CERN" = rec {
+    primary = false;
+    address  = "martin.bieker@cern.ch";
+    userName = "martin.bieker@cern.ch";
+
+
+    imap = {
+      host = "outlook.office365.com";
+      port = 993;
+      authentication = "xoauth2";
+      tls.enable = true;
+    };
+
+    smtp = {
+      host = "outlook.office365.com";
+      port = 587;
+      authentication = "xoauth2";
+      tls.enable = true;
+    };
+    signature = (mkSignature address);
+
+  } // common;
 
   programs.thunderbird = {
     enable = true;
